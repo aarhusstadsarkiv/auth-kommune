c70cd098401b6d0b0886f8852bd47939fd4d42d9
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-03-12 09:48:34 +0100

    user:User - add non-implemented properties

--- src/auth_kommune/user.py
+++ src/auth_kommune/user.py
@@ -9,3 +9,15 @@ class User(BaseUser):
         self.name: str = userinfo["name"]
         self.email: str = userinfo["email"]
         self.roles: list[str] = userinfo["access_groups"]
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def display_name(self) -> str:
+        return self.name
+
+    @property
+    def identity(self) -> str:
+        return self.id
