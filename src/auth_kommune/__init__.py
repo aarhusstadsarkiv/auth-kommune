74ba721914370c506978cc9234b1f1f14ca3fd58
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-03-12 09:52:31 +0100

    init - export pre-made authentication routes

--- src/auth_kommune/__init__.py
+++ src/auth_kommune/__init__.py
@@ -1,3 +1,5 @@
+from starlette.routing import Route
+
 from .authentication import handler_auth
 from .authentication import handler_login
 from .authentication import handler_logout
@@ -5,3 +7,20 @@ from .middleware import AccessLogMiddleware
 from .middleware import PostgreConnectionWrapper
 from .middleware import PostgresAuthenticationBackend
 from .user import User
+
+__all__ = [
+    "AccessLogMiddleware",
+    "PostgreConnectionWrapper",
+    "PostgresAuthenticationBackend",
+    "User",
+    "authentication_routes",
+    "handler_auth",
+    "handler_login",
+    "handler_logout",
+]
+
+authentication_routes: list[Route] = [
+    Route("/login", handler_login),
+    Route("/login/auth", handler_auth, name="auth"),
+    Route("/logout", handler_logout),
+]
