695cfc1581a310bce53668351fbd62bdac01d0c9
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-03-12 09:50:29 +0100

    init - export middleware and User classes

--- src/auth_kommune/__init__.py
+++ src/auth_kommune/__init__.py
@@ -1,3 +1,7 @@
 from .authentication import handler_auth
 from .authentication import handler_login
 from .authentication import handler_logout
+from .middleware import AccessLogMiddleware
+from .middleware import PostgreConnectionWrapper
+from .middleware import PostgresAuthenticationBackend
+from .user import User
