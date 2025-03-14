c8800567af85fa8816024ee18eb1664c16bedc1d
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-03-14 11:25:08 +0100

    user:User - add class variables to specify the keys used to get data from the userinfo object

--- src/auth_kommune/user.py
+++ src/auth_kommune/user.py
@@ -4,11 +4,16 @@ from starlette.authentication import BaseUser
 
 
 class User(BaseUser):
+    key_id: str = "id"
+    key_name: str = "name"
+    key_email: str = "email"
+    key_roles: str = "role"
+
     def __init__(self, userinfo: dict[str, Any]):
-        self.id: str = userinfo["id"]
-        self.name: str = userinfo["name"]
-        self.email: str = userinfo["email"]
-        self.roles: list[str] = userinfo["access_groups"]
+        self.id: str = userinfo[self.key_id]
+        self.name: str = userinfo[self.key_name]
+        self.email: str = userinfo[self.key_email]
+        self.roles: list[str] = userinfo[self.key_roles]
 
     @property
     def is_authenticated(self):
